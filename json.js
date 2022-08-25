d9964c0e47d52b9250e143d5520f2354b779af00
Leon9708 <Leon.ring@gmx.de>
2022-08-25 18:08:04 +0200

    second commit

--- json.js
+++ json.js
@@ -1,1 +1,41 @@
- 
+const questions = [{
+    "image": "./img/rules.jpg",
+    "question": "Aus wie vielen Zeichen besteht der Hex Color Code?",
+    "answer1": "6",
+    "answer2": "7",
+    "answer3": "5",
+    "answer4": "8",
+    "rightAnswer": "1"
+}, {
+    "image": "./img/blur.jpg",
+    "question": "Mit welchem Befehl kann man in dem Bild zu sehenden Effekt umsetzten ?",
+    "answer1": "blur()",
+    "answer2": "brightness()",
+    "answer3": "saturate()",
+    "answer4": "sepia()",
+    "rightAnswer": "1"
+}, {
+    "image": "./img/rules.jpg",
+    "question": "Was ist der Unterschied zwischen Grid und Flexbox ?",
+    "answer1": "Flexbox ist 2-Dimensional nutzbar",
+    "answer2": "Flexbox ist vielseitiger",
+    "answer3": "Grid ist besser bei Überlappungen nutzbar",
+    "answer4": "Flexbox verwendet man für Layouts",
+    "rightAnswer": "3"
+}, {
+    "image": "./img/box-shadow.jpg",
+    "question": "Wie setzt man einen Box-shadow-bottom?",
+    "answer1": "box-shadow: 0 5px 3px -3px",
+    "answer2": "box-shadow: 5px 0 3px -3px",
+    "answer3": "box-shadow: 0 -5px -3px -3px",
+    "answer4": "box-shadow: -5px 0 3px -3px",
+    "rightAnswer": "1",
+}, {
+    "image": "./img/rules.jpg",
+    "question": "Welche Methode kann ich alternative eines For-Loops verweden?",
+    "answer1": "keys()",
+    "answer2": "forEach()",
+    "answer3": "filter()",
+    "answer4": "join()",
+    "rightAnswer": "2"
+}]
--- main.js
+++ main.js
@@ -1,45 +1,3 @@
-const questions = [{
-    "image": "./img/rules.jpg",
-    "question": "Aus wie vielen Zeichen besteht der Hex Color Code?",
-    "answer1": "6",
-    "answer2": "7",
-    "answer3": "5",
-    "answer4": "8",
-    "rightAnswer": "1"
-}, {
-    "image": "./img/blur.jpg",
-    "question": "Mit welchem Befehl kann man in dem Bild zu sehenden Effekt umsetzten ?",
-    "answer1": "blur()",
-    "answer2": "brightness()",
-    "answer3": "saturate()",
-    "answer4": "sepia()",
-    "rightAnswer": "1"
-}, {
-    "image": "./img/rules.jpg",
-    "question": "Was ist der Unterschied zwischen Grid und Flexbox ?",
-    "answer1": "Flexbox ist 2-Dimensional nutzbar",
-    "answer2": "Flexbox ist vielseitiger",
-    "answer3": "Grid ist besser bei Überlappungen nutzbar",
-    "answer4": "Flexbox verwendet man für Layouts",
-    "rightAnswer": "3"
-}, {
-    "image": "./img/box-shadow.jpg",
-    "question": "Wie setzt man einen Box-shadow-bottom?",
-    "answer1": "box-shadow: 0 5px 3px -3px",
-    "answer2": "box-shadow: 5px 0 3px -3px",
-    "answer3": "box-shadow: 0 -5px -3px -3px",
-    "answer4": "box-shadow: -5px 0 3px -3px",
-    "rightAnswer": "1",
-}, {
-    "image": "./img/rules.jpg",
-    "question": "Welche Methode kann ich alternative eines For-Loops verweden?",
-    "answer1": "keys()",
-    "answer2": "forEach()",
-    "answer3": "filter()",
-    "answer4": "join()",
-    "rightAnswer": "2"
-}]
-
 let quiz = document.getElementById('quiz');
 let nextQuiz = document.getElementById('nextQuiz');
 let startEndGame = document.getElementById('startEndGame');
@@ -48,16 +6,11 @@ let previousQuestion = -1;
 let nextQuestionn = 1;
 let questionNumber = 1;
 let points = 0;
-let answer1 = document.getElementById('answer1');
-let answer2 = document.getElementById('answer2');
-let answer3 = document.getElementById('answer3');
-let answer4 = document.getElementById('answer4');
-
-
 
 
 function startGame() {
     document.getElementById('placeQuiz').classList.remove('d-none');
+    document.getElementById('quiz').classList.remove('d-none');
     startEndGame.classList.add('d-none');
 
     generateQuiz();
@@ -70,28 +23,31 @@ function nextQuestion() {
     if (questions.length !== questionNumber) {
         questionNumber++;
         currentQuestion++;
-        console.log(currentQuestion)
         generateQuiz();
         progressBar();
 
     } else {
         loadEndScreen();
     }
-    loadPreviousNext();
+    loadPrevious();
+    loadNext();
 }
 
 
-function loadPreviousNext() {
+function loadPrevious() {
+    document.getElementById('previousQuiz').classList.remove('quiz_box_transparent');
+    previousQuestion++;
+    generatePreviousQuiz();
+}
+
+
+function loadNext() {
     if (questions.length - 1 !== currentQuestion) {
         nextQuestionn++;
         generateNextQuiz();
     } else {
         document.getElementById('nextQuiz').classList.add('d-none');
     }
-
-    document.getElementById('previousQuiz').classList.remove('quiz_box_transparent');
-    previousQuestion++;
-    generatePreviousQuiz();
 }
 
 
@@ -100,24 +56,6 @@ function answer(choice) {
     let rightAnswerId = `answer${quizTag['rightAnswer']}`;
     let rightAnswer = quizTag['rightAnswer'];
 
-    if (choice === quizTag['answer1'].id) {
-        answer2.disable = true;
-        answer3.disable = true;
-        answer4.disable = true;
-    } else if (choice === quizTag['answer2'].id) {
-        answer1.disable = true;
-        answer3.disable = true;
-        answer4.disable = true;
-    } else if (choice === quizTag['answer3'].id) {
-        answer1.disable = true;
-        answer2.disable = true;
-        answer4.disable = true;
-    } else if (choice === quizTag['answer4'].id) {
-        answer1.disable = true;
-        answer2.disable = true;
-        answer3.disable = true;
-    }
-
     if (rightAnswer === selectedAnswer) {
         points++;
         document.getElementById(choice).classList.add("answer_right");
@@ -125,16 +63,25 @@ function answer(choice) {
         document.getElementById(choice).classList.add("answer_wrong");
         document.getElementById(rightAnswerId).classList.add("answer_right");
     }
+
+    coverAnswer();
+}
+
+
+function coverAnswer() {
+    document.getElementById('coverAnswers').classList.add('cover_answers')
     document.getElementById('nextBtn').disabled = false;
 }
 
 
 function loadEndScreen() {
     document.getElementById('placeQuiz').classList.add('d-none');
+    document.getElementById('quiz').classList.add('d-none');
 
     startEndGame.classList.remove('d-none');
     startEndGame.innerHTML = "";
     startEndGame.innerHTML = endScreenHtml();
+
     document.getElementById('reachedPoints').innerHTML = points;
     document.getElementById('reachedLength').innerHTML = questions.length;
 }
@@ -200,7 +147,7 @@ function endScreenHtml() {
     return `
     <div class="box_start_end_game">
         <img class="start_end_game_img" src="./img/done.jpg">
-        <div>
+        <div class="box_endmessage">
             <p class="counter_question game_end_points"> You have answered <span id="reachedPoints" class="counter"></span>  out of <span class="counter" id="reachedLength"></span> questions correctly</p>
             <p class="game_end_text">${advise()}</p>
         </div>
@@ -217,35 +164,36 @@ function quizHtml() {
             <div class="box_question">
                 <p class="question_text">${quizTag['question']}</p>
             </div>
-            <div class="box_place_answers">
-                <button id="answer1" onclick="answer('answer1')" class="answer">
-                    <div class="answer_num">A</div>
-                    <div class="answer_text">
-                        ${quizTag['answer1']}
-                    </div>
-                </button>
-                <button id="answer2" onclick="answer('answer2')" class="answer">
-                    <div class="answer_num">B</div>
-                    <div class="answer_text">
-                        ${quizTag['answer2']}
-                    </div>
-                </button>
-                <button id="answer3" onclick="answer('answer3')" class="answer">
-                    <div class="answer_num">C</div>
-                    <div class="answer_text">
-                        ${quizTag['answer3']}
-                    </div>
-                </button>
-                <button id="answer4" onclick="answer('answer4')" class="answer">
-                    <div class="answer_num">D</div>
-                    <div class="answer_text">
-                        ${quizTag['answer4']}
-                    </div>
-                </button>
-            </div>
+                <div class="box_place_answers">
+                    <button id="answer1" onclick="answer('answer1')" class="answer">
+                        <div class="answer_num">A</div>
+                        <div class="answer_text">
+                            ${quizTag['answer1']}
+                        </div>
+                    </button>
+                    <button id="answer2" onclick="answer('answer2')" class="answer">
+                        <div class="answer_num">B</div>
+                        <div class="answer_text">
+                            ${quizTag['answer2']}
+                        </div>
+                    </button>
+                    <button id="answer3" onclick="answer('answer3')" class="answer">
+                        <div class="answer_num">C</div>
+                        <div class="answer_text">
+                            ${quizTag['answer3']}
+                        </div>
+                    </button>
+                    <button id="answer4" onclick="answer('answer4')" class="answer">
+                        <div class="answer_num">D</div>
+                        <div class="answer_text">
+                            ${quizTag['answer4']}
+                        </div>
+                    </button>
+                    <div id="coverAnswers" ></div>
+                </div>
             <div class="place_back_next">
               <div>
-                <p class="counter_question"><span id="currentCounter"   class="counter">${questionNumber}</span>out of<span id="counterLength" class="counter"></span>Questions
+                <p class="counter_question"><span id="currentCounter"   class="counter">${questionNumber}</span>out of<span id="counterLength" class="counter"></span><span class="questions">Questions</span>
                 </p>
               </div>
                 <button id="nextBtn" onclick="nextQuestion()" disabled class="btn_back_next">
@@ -270,19 +218,19 @@ function nextQuizHtml(nextQuizTag) {
                </div>
            </div>
            <div class="answer_s">
-               <div class="answer_num_s">A</div>
+               <div class="answer_num_s">B</div>
                <div class="answer_text_s">
                    ${nextQuizTag['answer2']}
                </div>
            </div>
            <div class="answer_s">
-               <div class="answer_num_s">A</div>
+               <div class="answer_num_s">C</div>
                <div class="answer_text_s">
                    ${nextQuizTag['answer3']}
                </div>
            </div>
            <div class="answer_s">
-               <div class="answer_num_s">A</div>
+               <div class="answer_num_s">D</div>
                <div class="answer_text_s">
                    ${nextQuizTag['answer4']}
                </div>
@@ -308,19 +256,19 @@ function preQuizHtml(preQuizTag) {
                </div>
            </div>
            <div class="answer_s">
-               <div class="answer_num_s">A</div>
+               <div class="answer_num_s">B</div>
                <div class="answer_text_s">
                    ${preQuizTag['answer2']}
                </div>
            </div>
            <div class="answer_s">
-               <div class="answer_num_s">A</div>
+               <div class="answer_num_s">C</div>
                <div class="answer_text_s">
                    ${preQuizTag['answer3']}
                </div>
            </div>
            <div class="answer_s">
-               <div class="answer_num_s">A</div>
+               <div class="answer_num_s">D</div>
                <div class="answer_text_s">
                    ${preQuizTag['answer4']}
                </div>
